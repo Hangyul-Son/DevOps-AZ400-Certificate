a0e835ec546375c21dbd9f9cd5e24510a86a1a2d
Han <hangyulson1004@gmail.com>
2024-07-14 14:53:54 +0700

    Add app.db to .gitignore and create migrations folder

--- app/models.py
+++ app/models.py
@@ -4,3 +4,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    nationality = db.Column(db.String(120), unique=False, nullable=True)
+
+
